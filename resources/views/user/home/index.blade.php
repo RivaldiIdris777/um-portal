6065e599f2572c7fad22f3917c880367357f6ef7
RivaldiIdris777 <flyingriders905@gmail.com>
2024-05-14 12:46:36 +0700

    Penambahan tutorial tata cara pendaftaran

--- resources/views/user/home/index.blade.php
+++ resources/views/user/home/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layoutsfrontend.app')
 @section('contentfrontend')
 <!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex justify-content-center align-items-center" style="background: url('{{ Storage::url('public/slider/').$slider->gambar }}') top center; background-size: cover; max-width:100%;">
+<section id="hero" class="d-flex justify-content-center align-items-center"
+    style="background: url('{{ Storage::url('public/slider/').$slider->gambar }}') top center; background-size: cover; max-width:100%;">
     <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
         <h1>Berprestasi<br><span style="color:red;"> Bersama UM Jambi</span></h1>
         <h2>Terakreditasi Baik Sekali</h2>
@@ -40,29 +41,29 @@
             <div class="row counters">
 
                 @forelse ( $kapasitas as $kp )
-                    <div class="col-lg-3 col-6 text-center">
-                        <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_mahasiswa }}" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Mahasiswa</p>
-                    </div>
+                <div class="col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_mahasiswa }}"
+                        data-purecounter-duration="1" class="purecounter"></span>
+                    <p>Mahasiswa</p>
+                </div>
 
-                    <div class="col-lg-3 col-6 text-center">
-                        <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_fakultas }}" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Fakultas</p>
-                    </div>
+                <div class="col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_fakultas }}"
+                        data-purecounter-duration="1" class="purecounter"></span>
+                    <p>Fakultas</p>
+                </div>
 
-                    <div class="col-lg-3 col-6 text-center">
-                        <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_prodi }}" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Program Studi</p>
-                    </div>
+                <div class="col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_prodi }}"
+                        data-purecounter-duration="1" class="purecounter"></span>
+                    <p>Program Studi</p>
+                </div>
 
-                    <div class="col-lg-3 col-6 text-center">
-                        <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_dosen }}" data-purecounter-duration="1"
-                            class="purecounter"></span>
-                        <p>Dosen</p>
-                    </div>
+                <div class="col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="{{ $kp->jml_dosen }}"
+                        data-purecounter-duration="1" class="purecounter"></span>
+                    <p>Dosen</p>
+                </div>
 
                 @empty
 
@@ -83,32 +84,55 @@
                 </div>
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-3">
                     <div class="member">
-                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$jadwalpendaftaran->gambar }}" class="img-fluid rounded-3" alt="...">
+                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$jadwalpendaftaran->gambar }}"
+                            class="img-fluid rounded-3" alt="...">
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-3">
                     <div class="member">
-                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$fasilitas[0]->gambar }}" class="img-fluid rounded-3" alt="...">
+                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$fasilitas[0]->gambar }}"
+                            class="img-fluid rounded-3" alt="...">
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-3">
                     <div class="member">
-                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$jalurpendaftaran->gambar }}" class="img-fluid rounded-3" alt="...">
+                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$jalurpendaftaran->gambar }}"
+                            class="img-fluid rounded-3" alt="...">
                     </div>
                 </div>
 
                 {{-- <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-right" data-aos-delay="100">
-                    <img src="{{ Storage::url('public/jalur_pendaftaran/').$jadwalpendaftaran->gambar }}" class="img-fluid" alt="">
-                    <img src="{{ Storage::url('public/jalur_pendaftaran/').$fasilitas[0]->gambar }}" class="img-fluid" alt="">
+                    <img src="{{ Storage::url('public/jalur_pendaftaran/').$jadwalpendaftaran->gambar }}"
+                class="img-fluid" alt="">
+                <img src="{{ Storage::url('public/jalur_pendaftaran/').$fasilitas[0]->gambar }}" class="img-fluid"
+                    alt="">
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+                <h3>Jalur Pendaftaran </h3>
+                <p class="fst-justify">
+                    <img src="{{ Storage::url('public/jalur_pendaftaran/').$jalurpendaftaran->gambar }}"
+                        class="img-fluid" alt="">
+                </p>
+            </div> --}}
+        </div>
+
+        </div>
+    </section><!-- End About Section -->
+
+    <section id="tatacara-pendaftaran" class="about mt-5">
+        <div class="container">
+
+            <div class="row" data-aos="fade-up">
+                <div class="section-title">
+                    <p style="color:#2f3542">Tata Cara Pendaftaran Pendaftaran Mahasiswa Baru</p>
                 </div>
-                <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-                    <h3>Jalur Pendaftaran </h3>
-                    <p class="fst-justify">
-                        <img src="{{ Storage::url('public/jalur_pendaftaran/').$jalurpendaftaran->gambar }}" class="img-fluid" alt="">
-                    </p>
-                </div> --}}
+
+                <div class="col-lg-12 col-md-12">
+                    <iframe style="width: 100%;" height="700" src="https://www.youtube.com/embed/70CxG9-4Bjo?si=PoP634jfDr0hMKU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+
             </div>
 
         </div>
